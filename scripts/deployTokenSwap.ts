2a71139c0908448c95ec12aa01d0741cb40868be
havidtech <76119744+havidtech@users.noreply.github.com>
2022-03-15 22:02:03 +0100

    fix: update deploy script

--- scripts/deployTokenSwap.ts
+++ scripts/deployTokenSwap.ts
@@ -2,12 +2,19 @@ import { ethers } from "hardhat";
 
 async function deployTokenSwap() {
   // Tokens to form market
-  const token1Address = "token1Address";
-  const token2Address = "token2Address";
+  const token1Address = "token1Address"; // Change this
+  const token2Address = "token2Address"; // Change this
+  const token1ToEthFeed = "token1ToEthFeed"; // Change this
+  const token2ToEthFeed = "token2ToEthFeed"; // Change this
 
   // We get the contract to deploy
   const TokenSwap = await ethers.getContractFactory("TokenSwap");
-  const tokenSwap = await TokenSwap.deploy(token1Address, token2Address);
+  const tokenSwap = await TokenSwap.deploy(
+    token1Address,
+    token2Address,
+    token1ToEthFeed,
+    token2ToEthFeed
+  );
 
   await tokenSwap.deployed();
 
