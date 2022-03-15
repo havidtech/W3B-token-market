61ca0f0a50112d88828e2a9f3332987c5fced76c
havidtech <76119744+havidtech@users.noreply.github.com>
2022-03-15 21:10:03 +0100

    fix: implement contract functions

--- contracts/TokenSwap.sol
+++ contracts/TokenSwap.sol
@@ -3,6 +3,19 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
+interface IERCToken {
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address _owner) external view returns (uint256 balance);
+    function transfer(address _to, uint256 _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
+    function approve(address _spender, uint256 _value) external returns (bool success);
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
+}
+
+
 contract TokenSwap {
     address private token1Address;
     address private token2Address;
@@ -12,39 +25,40 @@ contract TokenSwap {
         token2Address = _token2Address;
     }
 
-    function swap(address _from, address _to, uint amount) public returns (bool){
+    function swap(address _sourceTokenAddress, address _destinationTokenAddress, uint amount) public returns (bool){
 
         // Assert that token is supported
+        require(isSwapValid(_sourceTokenAddress, _destinationTokenAddress), "This Swap is not supported in our market");
 
         // Require sufficient balance of initiator
-
-        // Require sufficient liquidity of the market
+        require(isBalanceSufficient(_sourceTokenAddress, msg.sender, amount), "Insufficient Balance");
 
         // Calculate amount of destination token
+        uint destinationAmount = 10; // Change this when chain link is integrated
 
-        // Do transfers accordingly
+        // Require sufficient liquidity of the market
+        require(isBalanceSufficient(_destinationTokenAddress, address(this), destinationAmount), "Insufficient Market Liquidity");
+
+        // Do swap accordingly
+        swapToken(_sourceTokenAddress, _destinationTokenAddress, msg.sender, amount, destinationAmount);
+
+        return true;
     }
 
-    function computeToAmount(address _fromToken, address _toToken, uint amount) public pure returns (uint) {
+    function computeToAmount(address _sourceToken, address _destinationToken, uint amount) public pure returns (uint) {
     }
 
-    function isFromBalanceSufficient(address _tokenAddress, uint amount) internal returns (bool){
-
+    function isBalanceSufficient(address _tokenAddress, address _owner, uint amount) internal view returns (bool){
+        IERCToken token = IERCToken(_tokenAddress);
+        return token.balanceOf(_owner) >= amount;
     }
 
-    function isMarketLiquiditySufficient(address _tokenAddress, uint amount) internal returns (bool) {
-
+    function isSwapValid(address _token1, address _token2) internal view returns (bool){
+        return (_token1 != _token2) && (_token1 == token1Address || _token1 == token2Address ) && (_token2 == token1Address || _token2 == token2Address );
     }
 
-    function isTokenSupported(address _token) internal returns (bool){
-
-    }
-
-    function transferFromToken(address _token, uint amount) internal returns (bool){
-
-    }
-
-    function transferToToken(address _token, address _to,  uint amount) internal returns (bool) {
-
+    function swapToken(address _sourceTokenAddress, address _destinationTokenAddress, address _initiator, uint _sourceAmount, uint _destinationAmount) internal{
+        IERCToken(_sourceTokenAddress).transferFrom(_initiator, address(this), _sourceAmount);
+        IERCToken(_destinationTokenAddress).transfer(_initiator, _destinationAmount);
     }
 }
